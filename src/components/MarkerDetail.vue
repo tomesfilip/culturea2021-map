0cb120e9361eac3367c3ee39d915d1721429f03d
tomesfilip <filiptomes@protonmail.com>
2021-04-04 19:56:58 +0200

    small styling changes

--- src/components/MarkerDetail.vue
+++ src/components/MarkerDetail.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-full h-full lg:h-52 bg-green lg:bg-white flex items-center sm:justify-center">
+  <div class="w-full h-full lg:h-56 bg-green lg:bg-white flex items-center sm:justify-center">
     <div class="p-4 flex w-full h-full sm:w-1/2 lg:w-full flex-col lg:flex-row text-center lg:text-left text-white lg:text-black">
       <h4 class="w-4/5 sm:w-full sm:text-center lg:w-1/6 mr-0 lg:mr-8 uppercase font-heading text-white lg:text-green self-start md:self-auto text-lg mobile:text-xl text-left">{{ mapItem.properties.name }}</h4>
       <div class="block lg:hidden close-btn fixed right-6 top-10">
         <slot name="mobCloseBtn"></slot>
       </div>
-      <div class="map-item-detail w-full lg:w-4/6 h-5/6 mt-8 lg:h-full flex flex-col justify-evenly">
+      <div class="map-item-detail w-full lg:w-4/6 h-5/6 mt-8 lg:mt-0 lg:h-full flex flex-col justify-evenly">
         <div class="text flex mb-0 lg:mb-4 flex-col lg:flex-row">
           <p class="mr-0 lg:mr-2 uppercase mobile:text-lg lg:text-base font-bold">detail</p>
           <p>{{ mapItem.properties.detail_text }}</p>
