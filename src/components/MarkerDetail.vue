e8083cd145bdcb1ce7a97ce4d4ce49a468505faf
f_tomes <f_tomes@utb.cz>
2021-05-09 09:33:35 +0200

    removed unnecessary js files

--- src/components/MarkerDetail.vue
+++ src/components/MarkerDetail.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="w-full h-full lg:h-56 bg-green lg:bg-white flex items-center sm:justify-center">
-    <div class="p-4 flex w-full h-full sm:w-1/2 lg:w-full flex-col lg:flex-row text-center lg:text-left text-white lg:text-black">
-      <h4 class="w-4/5 sm:w-full sm:text-center lg:w-1/6 mr-0 lg:mr-8 uppercase font-heading text-white lg:text-green self-start md:self-auto text-lg mobile:text-xl text-left">{{ mapItem.properties.name }}</h4>
+    <div class="p-4 flex w-full h-full sm:w-1/2 lg:w-full flex-col lg:flex-row text-center justify-evenly lg:text-left text-white lg:text-black">
+      <div class="headline w-4/6 sm:w-full lg:w-1/6 sm:text-center lg:text-left">
+        <h4 class="mr-0 lg:mr-8 uppercase font-heading text-white lg:text-green text-lg mobile:text-xl text-left">{{ mapItem.properties.name }}</h4>
+      </div>
       <div class="block lg:hidden close-btn fixed right-6 top-10">
         <slot name="mobCloseBtn"></slot>
       </div>
-      <div class="map-item-detail w-full lg:w-4/6 h-5/6 mt-8 lg:mt-0 lg:h-full flex flex-col justify-evenly">
+      <div class="map-item-detail w-full lg:w-4/6 h-5/6 lg:h-full flex flex-col justify-around">
         <div class="text flex mb-0 lg:mb-4 flex-col lg:flex-row">
           <p class="mr-0 lg:mr-2 uppercase mobile:text-lg lg:text-base font-bold">detail</p>
           <p>{{ mapItem.properties.detail_text }}</p>
